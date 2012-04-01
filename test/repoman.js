b3994e084ad688f578cca4335443848736902069
Cliffano Subagio <cliffano@gmail.com>
2012-04-01 20:02:33 +1000

    Use Cly to execute shell commands.

--- test/repoman.js
+++ test/repoman.js
@@ -10,13 +10,14 @@ vows.describe('repoman').addBatch({
       return function (checks) {
         return sandbox.require('../lib/repoman', {
           requires: {
-            child_process: {
-              exec: function (command, cb) {
+            cly: {
+              exec: function (command, fallthrough, cb) {
+                assert.isTrue(fallthrough);
                 // simulate success on git repo and failure on svn repo
                 if (command === 'git clone http://github.com/x/y /workspace/xy') {
-                  cb(null, 'success message');
+                  cb(null, { status: 'success', checks: checks });
                 } else if (command === 'svn checkout http://abc.googlecode.com /workspace/abc') {
-                  cb(new Error('dummyerror'), null, 'error message');
+                  cb(new Error('dummyerror'), { status: 'error', checks: checks });
                 }
               }
             }
@@ -25,9 +26,6 @@ vows.describe('repoman').addBatch({
             console: {
               log: function (message) {
                 checks.messages.log.push(message);
-              },
-              error: function (message) {
-                checks.messages.error.push(message);
               }
             }
           }
@@ -53,21 +51,20 @@ vows.describe('repoman').addBatch({
             }
           });
         repoman.run('init', this.callback);
-        assert.equal(checks.messages.log.length, 5);
-        assert.equal(checks.messages.log[0], '+ xy');
-        assert.equal(checks.messages.log[1], 'git clone http://github.com/x/y /workspace/xy');
-        assert.equal(checks.messages.log[2], 'success message');
-        assert.equal(checks.messages.log[3], '+ abc');
-        assert.equal(checks.messages.log[4], 'svn checkout http://abc.googlecode.com /workspace/abc');
-        assert.equal(checks.messages.error.length, 1);
-        assert.equal(checks.messages.error[0], 'error message');
       },
       'then successful command should have status ok': function (err, results) {
-        assert.isNull(err);
-        assert.equal(results.xy.status, 'ok');
+        assert.equal(results.xy.status, 'success');
+        assert.equal(results.abc.status, 'error');
       },
       'and failure command should pass correct error message': function (err, results) {
-        assert.equal(results.abc.message, 'dummyerror');
+        assert.equal(err.message, 'dummyerror');
+      },
+      'and log messages should be written to console': function (err, results) {
+        assert.equal(results.xy.checks.messages.log.length, 4);
+        assert.equal(results.xy.checks.messages.log[0], '+ xy');
+        assert.equal(results.xy.checks.messages.log[1], 'git clone http://github.com/x/y /workspace/xy');
+        assert.equal(results.xy.checks.messages.log[2], '+ abc');
+        assert.equal(results.xy.checks.messages.log[3], 'svn checkout http://abc.googlecode.com /workspace/abc');
       }
     }
   }
