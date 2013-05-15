4404e940833a7e0529891991b5080e0624c9524c
Cliffano Subagio <cliffano@gmail.com>
2013-05-15 23:22:38 +1000

    Add cli tests.

--- test/cli.js
+++ test/cli.js
@@ -1,6 +1,8 @@
 var bag = require('bagofholding'),
   buster = require('buster'),
   cli = require('../lib/cli'),
+  commander = require('commander'),
+  fs = require('fs'),
   Repoman = new require('../lib/repoman');
 
 buster.testCase('cli - exec', {
@@ -66,291 +68,194 @@ buster.testCase('cli - config', {
   }
 });
 
-/*
 buster.testCase('cli - _exec', {
-  'should execute custom command specified in arg if command is exec': function () {
-
+  setUp: function () {
+    this.mockFs = this.mock(fs);
+    this.mockProcess = this.mock(process);
   },
-  'should pass non-exec command as-is': function () {
-
+  'should execute custom command specified in arg if command is exec': function () {
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.exec.action('ls -al', { _name: 'exec', parent: {} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.repoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    this.mockFs.expects('readFileSync').returns('{}');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Repoman.prototype, 'exec', function (command, cb) {
+      assert.equals(command, 'ls -al');
+      cb();
+    });
+    cli.exec();
+  },
+  'should pass built-in command as-is': function () {
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.exec.action({ _name: 'init', parent: {} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.repoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    this.mockFs.expects('readFileSync').returns('{}');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Repoman.prototype, 'exec', function (command, cb) {
+      assert.equals(command, 'init');
+      cb();
+    });
+    cli.exec();
   },
   'should use win32 command when executed on windows': function () {
-
+    this.stub(process, 'platform', 'win32');
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.exec.action({ _name: 'init', parent: {} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.repoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    this.mockFs.expects('readFileSync').returns('{}');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Repoman.prototype, 'exec', function (command, cb) {
+      assert.equals(command, 'init');
+      cb();
+    });
+    cli.exec();
+  },
+  'should use custom config file when specified in args': function () {
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.exec.action({ _name: 'init', parent: { configFile: '.somerepoman.json'} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.somerepoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    this.mockFs.expects('readFileSync').returns('{}');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Repoman.prototype, 'exec', function (command, cb) {
+      assert.equals(command, 'init');
+      cb();
+    });
+    cli.exec();
   }
 });
 
 buster.testCase('cli - clean', {
+  setUp: function () {
+    this.mockCommander = this.mock(commander);
+    this.mockConsole = this.mock(console);
+    this.mockProcess = this.mock(process);
+  },
   'should delete nothing when there is no directory to clean up': function () {
-
-  },
-  'should delete directories to clean up when user confirms to do so': function () {
-
-  },
-  'should not delete directories to clean up when user confirms to not clean': function () {
-
-  },
-  'should pass error to callback when there is an error during dry run': function () {
-
-  }
-});
-*/
-
-/*
-var bag = require('bagofholding'),
-  sandbox = require('sandboxed-module'),
-  should = require('should'),
-  checks, mocks,
-  cli;
-
-describe('cli', function () {
-
-  function create(checks, mocks) {
-    return sandbox.require('../lib/cli', {
-      requires: {
-        bagofholding: {
-          cli: {
-            exit: function (err, result) {
-              checks.bag_cli_exit_err = err;
-              checks.bag_cli_exit_result = result;
-            },
-            parse: function (commands, dir) {
-              checks.bag_parse_commands = commands;
-              checks.bag_parse_dir = dir;
-            },
-            readConfigFileSync: function (file) {
-              return mocks['fs_readFileSync_/someuserhome/.repoman.json'];
-            },
-            readCustomConfigFileSync: function (file) {
-              return mocks['fs_readFileSync_/curr/dir/.custom.repoman.json'];
-            }
-          }
-        },
-        commander: {
-          prompt: function (question, cb) {
-            checks.commander_prompt_question = question;
-            cb(mocks.commander_prompt_answer);
-          }
-        },
-        fs: bag.mock.fs(checks, mocks),
-        './repoman': function (config, scms) {
-          checks.repoman_config = config;
-          return {
-            clean: function (dryRun, cb) {
-              if (dryRun) {
-                cb(mocks.clean_dryRun_err,
-                  mocks.clean_dryRun_result);
-              } else {
-                cb(mocks.clean_nonDryRun_err,
-                  mocks.clean_nonDryRun_result);
-              }
-            },
-            config: function (opts, exit) {
-              checks.repoman_config_opts = opts;
-              checks.repoman_config_exit = exit;
-            },
-            exec: function (command, exit) {
-              checks.repoman_run_command = command;
-              checks.repoman_run_exit = exit;
-            }
-          };
-        }
+    this.mockConsole.expects('log').once().withExactArgs('Nothing to delete.');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.stub(Repoman.prototype, 'clean', function (dryRun, cb) {
+      assert.equals(dryRun, true);
+      cb(null, null);
+    });
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json'} });
       },
-      globals: {
-        process: bag.mock.process(checks, mocks),
-        console: bag.mock.console(checks, mocks)
-      },
-      locals: {
-        __dirname: '/somedir/repoman/lib'
+      lookupFile: function (file) {
+        assert.equals(file, '.somerepoman.json');
+        return '{}';
       }
     });
+    cli.exec();
+  },
+  'should delete directories to clean up when user confirms to do so': function () {
+    this.mockConsole.expects('log').once().withExactArgs('The following files/directories will be deleted: %s', 'dir1, dir2');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.mockCommander.expects('prompt').once().withArgs('Are you sure? (Y/N)').callsArgWith(1, 'Y');
+    this.stub(Repoman.prototype, 'clean', function (dryRun, cb) {
+      if (dryRun) {
+        cb(null, ['dir1', 'dir2']);
+      } else {
+        cb();
+      }
+    });
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json'} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.somerepoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    cli.exec();
+  },
+  'should not delete directories to clean up when user confirms to not clean': function () {
+    this.mockConsole.expects('log').once().withExactArgs('The following files/directories will be deleted: %s', 'dir1, dir2');
+    this.mockConsole.expects('log').once().withExactArgs('Nothing is deleted.');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.mockCommander.expects('prompt').once().withArgs('Are you sure? (Y/N)').callsArgWith(1, 'N');
+    this.stub(Repoman.prototype, 'clean', function (dryRun, cb) {
+      assert.equals(dryRun, true);
+      cb(null, ['dir1', 'dir2']);
+    });
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json'} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.somerepoman.json');
+        return '{}';
+      }
+    });
+    cli.exec();
+  },
+  'should not delete directories to clean up when user does not provide confirmation answer': function () {
+    this.mockConsole.expects('log').once().withExactArgs('The following files/directories will be deleted: %s', 'dir1, dir2');
+    this.mockConsole.expects('log').once().withExactArgs('Nothing is deleted.');
+    this.mockProcess.expects('exit').once().withExactArgs(0);
+    this.mockCommander.expects('prompt').once().withArgs('Are you sure? (Y/N)').callsArgWith(1, '');
+    this.stub(Repoman.prototype, 'clean', function (dryRun, cb) {
+      assert.equals(dryRun, true);
+      cb(null, ['dir1', 'dir2']);
+    });
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json'} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.somerepoman.json');
+        return '{}';
+      }
+    });
+    cli.exec();
+  },
+  'should pass error to callback when there is an error during dry run': function () {
+    this.mockConsole.expects('error').once().withExactArgs('some error');
+    this.mockProcess.expects('exit').once().withExactArgs(1);
+    this.stub(Repoman.prototype, 'clean', function (dryRun, cb) {
+      assert.equals(dryRun, true);
+      cb(new Error('some error'));
+    });
+    this.stub(bag, 'cli', {
+      command: function (base, actions) {
+        actions.commands.clean.action({ _name: 'clean', parent: {} });
+      },
+      lookupFile: function (file) {
+        assert.equals(file, '.repoman.json');
+        return '{}';
+      },
+      exit: bag.cli.exit
+    });
+    cli.exec();
   }
-
-  beforeEach(function () {
-    checks = {};
-    mocks = {};
-  });
-
-  describe('exec', function () {
-
-    beforeEach(function () {
-      mocks = {
-        process_cwd: '/curr/dir/',
-        'fs_readFileSync_/somedir/repoman/conf/scms.json': '{}',
-        'fs_readFileSync_/someuserhome/.repoman.json': '{}',
-        'fs_readFileSync_/curr/dir/.custom.repoman.json': '{ "customfoo": "custombar" }'
-      };
-      cli = create(checks, mocks);
-      cli.exec();
-    });
-
-    afterEach(function () {
-      checks.bag_parse_dir.should.equal('/somedir/repoman/lib');
-    });
-
-    it('should contain config command and delegate to repoman config when exec is called', function () {
-      checks.bag_parse_commands.config.desc.should.equal('Create configuration file');
-      checks.bag_parse_commands.config.options.length.should.equal(4);
-      checks.bag_parse_commands.config.options[0].arg.should.equal('--github-user <githubUser>');
-      checks.bag_parse_commands.config.options[0].desc.should.equal('Comma-separated GitHub usernames');
-      checks.bag_parse_commands.config.options[1].arg.should.equal('--github-org <githubOrg>');
-      checks.bag_parse_commands.config.options[1].desc.should.equal('Comma-separated GitHub organisations');
-      checks.bag_parse_commands.config.options[2].arg.should.equal('--github-auth-user <githubAuthUser>');
-      checks.bag_parse_commands.config.options[2].desc.should.equal('GitHub authentication username');
-      checks.bag_parse_commands.config.options[3].arg.should.equal('--github-auth-pass <githubAuthPass>');
-      checks.bag_parse_commands.config.options[3].desc.should.equal('GitHub authentication password');
-
-      // without any flag
-      checks.bag_parse_commands.config.action({});
-
-      // with github flags
-      checks.bag_parse_commands.config.action({
-        githubUser: 'somegithubuser',
-        githubOrg: 'somegithuborg',
-        githubAuthUser: 'somegithubauthuser',
-        githubAuthPass: 'somegithubauthpass'
-      });
-      checks.repoman_config_opts.github.user.should.equal('somegithubuser');
-      checks.repoman_config_opts.github.org.should.equal('somegithuborg');
-      checks.repoman_config_opts.github.auth.user.should.equal('somegithubauthuser');
-      checks.repoman_config_opts.github.auth.pass.should.equal('somegithubauthpass');
-
-      checks.repoman_config_exit.should.be.a('function');
-      should.not.exist(checks.fs_readFileSync_file);
-    });
-
-    it('should contain delete command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands['delete'].desc.should.equal('Delete the local repositories');
-      checks.bag_parse_commands['delete'].action({ name: 'delete', parent: {} });
-      checks.repoman_run_command.should.equal('delete');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should contain init command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands.init.desc.should.equal('Initialise local repositories');
-      checks.bag_parse_commands.init.action({ name: 'init', parent: {} });
-      checks.repoman_run_command.should.equal('init');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should use custom config file when config arg is specified', function () {
-      checks.bag_parse_commands.init.desc.should.equal('Initialise local repositories');
-      checks.bag_parse_commands.init.action({ name: 'init', parent: { configFile: '.custom.repoman.json' } });
-      checks.repoman_config.customfoo.should.equal('custombar');
-    });
-
-    it('should contain get command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands.get.desc.should.equal('Update local repositories with changes from remote repositories');
-      checks.bag_parse_commands.get.action({ name: 'get', parent: {} });
-      checks.repoman_run_command.should.equal('get');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should contain changes command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands.changes.desc.should.equal('Display the changes in local repositories');
-      checks.bag_parse_commands.changes.action({ name: 'changes', parent: {} });
-      checks.repoman_run_command.should.equal('changes');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should contain save command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands.save.desc.should.equal('Update remote repositories with changes from local repositories');
-      checks.bag_parse_commands.save.action({ name: 'save', parent: {} });
-      checks.repoman_run_command.should.equal('save');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should contain undo command and delegate to repoman exec when exec is called', function () {
-      checks.bag_parse_commands.undo.desc.should.equal('Remove uncommitted changes from local repositories');
-      checks.bag_parse_commands.undo.action({ name: 'undo', parent: {} });
-      checks.repoman_run_command.should.equal('undo');
-      checks.repoman_run_exit.should.be.a('function');
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms.json');
-    });
-
-    it('should contain command and use windows-specific config file when platform is windows', function () {
-      mocks = {
-        process_platform: 'win32',
-        'fs_readFileSync_/somedir/repoman/conf/scms-win32.json': '{}',
-        'fs_readFileSync_/someuserhome/.repoman.json': '{}'
-      };
-      cli = create(checks, mocks);
-      cli.exec();
-      checks.bag_parse_commands['delete'].action({ name: 'delete', parent: {} });
-      checks.fs_readFileSync_file.should.equal('/somedir/repoman/conf/scms-win32.json');
-    });
-
-    it('should contain exec command and delegate to repoman exec with first argument as the command', function () {
-      checks.bag_parse_commands.exec.desc.should.equal('Execute custom command against local repositories');
-      checks.bag_parse_commands.exec.action('touch .gitignore;', { name: 'exec', parent: {} });
-      checks.repoman_run_command.should.equal('touch .gitignore;');
-      checks.repoman_run_exit.should.be.a('function');
-    });
-
-    it('should contain clean command and delegate to repoman exec with first argument as the command', function () {
-      checks.bag_parse_commands.clean.desc.should.equal('Delete non-Repoman local repositories');
-      checks.bag_parse_commands.clean.action({ name: 'clean', parent: {} });
-    });
-  });
-
-  describe('clean', function () {
-
-    beforeEach(function () {
-      mocks = {
-        process_cwd: '/curr/dir/',
-        'fs_readFileSync_/someuserhome/.repoman.json': '{}'
-      };
-    });
-
-    it('should pass error when dry run clean returns an error', function () {
-      mocks.clean_dryRun_err = new Error('someerror');
-      cli = create(checks, mocks);
-      cli.exec();
-      checks.bag_parse_commands.clean.action({ parent: {}});
-      checks.bag_cli_exit_err.message.should.equal('someerror');
-      should.not.exist(checks.bag_cli_exit_result);
-    });
-
-    it('should display nothing to delete message when dry run returns empty result', function () {
-      mocks.clean_dryRun_result = [];
-      cli = create(checks, mocks);
-      cli.exec();
-      checks.bag_parse_commands.clean.action({ parent: {}});
-      checks.process_exit_code.should.equal(0);
-      checks.console_log_messages.length.should.equal(1);
-      checks.console_log_messages[0].should.equal('Nothing to delete.');
-    });
-
-    it('should display nothing is deleted when confirmation answer is not Y', function () {
-      mocks.clean_dryRun_result = ['foo', 'bar'];
-      mocks.commander_prompt_answer = 'N';
-      cli = create(checks, mocks);
-      cli.exec();
-      checks.bag_parse_commands.clean.action({ parent: {}});
-      checks.commander_prompt_question.should.equal('Are you sure? (Y/N)');
-      checks.process_exit_code.should.equal(0);
-      checks.console_log_messages.length.should.equal(2);
-      checks.console_log_messages[0].should.equal('The following files/directories will be deleted: foo, bar');
-      checks.console_log_messages[1].should.equal('Nothing is deleted.');
-    });
-
-    it('should call non dry run clean when confirmation answer is Y', function () {
-      mocks.clean_dryRun_result = ['foo', 'bar'];
-      mocks.clean_nonDryRun_result = 'dummyresult';
-      mocks.commander_prompt_answer = 'Y';
-      cli = create(checks, mocks);
-      cli.exec();
-      checks.bag_parse_commands.clean.action({ parent: {}});
-      checks.commander_prompt_question.should.equal('Are you sure? (Y/N)');
-      checks.console_log_messages.length.should.equal(1);
-      checks.console_log_messages[0].should.equal('The following files/directories will be deleted: foo, bar');
-      checks.bag_cli_exit_result.should.equal('dummyresult');
-    });
-  });
 });
- 
-*/
