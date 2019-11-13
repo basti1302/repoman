108b30669fd9d5be5d923799c09f6828b6fe61f2
Bastian Krol <bastian.krol@web.de>
2019-11-13 22:59:13 +0100

    format everything with prettier

--- test/cli.js
+++ test/cli.js
@@ -13,11 +13,10 @@ var sinon = require('sinon');
 var assert = chai.assert;
 
 describe('cli', function() {
-
   var commandStub;
   var lookupFileStub;
 
-  beforeEach(function () {
+  beforeEach(function() {
     sinon.spy(console, 'log');
     sinon.spy(console, 'error');
     sinon.stub(process, 'exit');
@@ -25,7 +24,7 @@ describe('cli', function() {
     lookupFileStub = sinon.stub(bag, 'lookupFile');
   });
 
-  afterEach(function () {
+  afterEach(function() {
     console.log.restore();
     console.error.restore();
     process.exit.restore();
@@ -34,9 +33,8 @@ describe('cli', function() {
   });
 
   describe('exec', function() {
-
-    it('should contain commands with actions', function (done) {
-      var mockCommand = function (base, actions) {
+    it('should contain commands with actions', function(done) {
+      var mockCommand = function(base, actions) {
         assert.isDefined(base);
         assert.isDefined(actions.commands.config.action);
         assert.isDefined(actions.commands['delete'].action);
@@ -55,24 +53,23 @@ describe('cli', function() {
   });
 
   describe('config', function() {
-
     var configStub;
 
-    beforeEach(function () {
+    beforeEach(function() {
       sinon.stub(process, 'cwd');
       configStub = sinon.stub(Repoman.prototype, 'config');
     });
 
-    afterEach(function () {
+    afterEach(function() {
       process.cwd.restore();
       Repoman.prototype.config.restore();
     });
 
-    it('should pass empty opts when there is no arg', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should pass empty opts when there is no arg', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.config.action({});
       });
-      configStub.callsFake(function (opts, cb) {
+      configStub.callsFake(function(opts, cb) {
         assert.isEmpty(opts);
         cb();
       });
@@ -80,14 +77,14 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass bitbucket opts when specified in args', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should pass bitbucket opts when specified in args', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.config.action({
           bitbucketAuthUser: 'someauthuser',
           bitbucketAuthPass: 'someauthpass'
         });
       });
-      configStub.callsFake(function (opts, cb) {
+      configStub.callsFake(function(opts, cb) {
         assert.equal(opts.bitbucket.authUser, 'someauthuser');
         assert.equal(opts.bitbucket.authPass, 'someauthpass');
         cb();
@@ -96,8 +93,8 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass github opts when specified in args', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should pass github opts when specified in args', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.config.action({
           githubUser: 'someuser',
           githubOrg: 'someorg',
@@ -105,7 +102,7 @@ describe('cli', function() {
           githubAuthPass: 'someauthpass'
         });
       });
-      configStub.callsFake(function (opts, cb) {
+      configStub.callsFake(function(opts, cb) {
         assert.equal(opts.github.user, 'someuser');
         assert.equal(opts.github.org, 'someorg');
         assert.equal(opts.github.authUser, 'someauthuser');
@@ -116,14 +113,14 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass gitorious opts when specified in args', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should pass gitorious opts when specified in args', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.config.action({
           gitoriousUrl: 'http://somehost.com',
           gitoriousProject: 'someproject'
         });
       });
-      configStub.callsFake(function (opts, cb) {
+      configStub.callsFake(function(opts, cb) {
         assert.equal(opts.gitorious.url, 'http://somehost.com');
         assert.equal(opts.gitorious.project, 'someproject');
         cb();
@@ -132,14 +129,14 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass local opts when specified in args', function () {
+    it('should pass local opts when specified in args', function() {
       process.cwd.returns('somedir');
-      commandStub.callsFake(function (base, actions) {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.config.action({
           local: true
         });
       });
-      configStub.callsFake(function (opts, cb) {
+      configStub.callsFake(function(opts, cb) {
         assert.equal(opts.local.dir, 'somedir');
         cb();
       });
@@ -149,31 +146,30 @@ describe('cli', function() {
   });
 
   describe('_exec', function() {
-
     var execStub;
     var mockFs;
 
-    beforeEach(function () {
+    beforeEach(function() {
       mockFs = sinon.mock(fs);
       execStub = sinon.stub(Repoman.prototype, 'exec');
     });
 
-    afterEach(function () {
+    afterEach(function() {
       Repoman.prototype.exec.restore();
       mockFs.verify();
       mockFs.restore();
     });
 
-    it('should execute custom command specified in arg if command is exec', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should execute custom command specified in arg if command is exec', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.exec.action('ls -al', { _name: 'exec', parent: {} });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.repoman.json');
         return '{}';
       });
       mockFs.expects('readFileSync').returns('{}');
-      execStub.callsFake(function (command, opts, cb) {
+      execStub.callsFake(function(command, opts, cb) {
         assert.equal(command, 'ls -al');
         assert.equal(opts.failFast, undefined);
         assert.equal(opts.regex, undefined);
@@ -184,16 +180,16 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass built-in command as-is', function () {
-      commandStub.callsFake(function (base, actions) {
+    it('should pass built-in command as-is', function() {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.exec.action({ _name: 'init', parent: {} });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.repoman.json');
         return '{}';
       });
       mockFs.expects('readFileSync').returns('{}');
-      execStub.callsFake(function (command, opts, cb) {
+      execStub.callsFake(function(command, opts, cb) {
         assert.equal(command, 'init');
         assert.equal(opts.failFast, undefined);
         assert.equal(opts.regex, undefined);
@@ -204,16 +200,19 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should use win32 command when executed on windows', function () {
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.exec.action({ _name: 'init', parent: { platform: 'win32' } });
+    it('should use win32 command when executed on windows', function() {
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.exec.action({
+          _name: 'init',
+          parent: { platform: 'win32' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.repoman.json');
         return '{}';
       });
       mockFs.expects('readFileSync').returns('{}');
-      execStub.callsFake(function (command, opts, cb) {
+      execStub.callsFake(function(command, opts, cb) {
         assert.equal(command, 'init');
         assert.equal(opts.failFast, undefined);
         assert.equal(opts.regex, undefined);
@@ -224,16 +223,24 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should use custom config file when specified in args', function () {
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.exec.action({ _name: 'init', parent: { failFast: true, regex: '.*github.*', tags: 'tag1,tag2', configFile: '.somerepoman.json' } });
+    it('should use custom config file when specified in args', function() {
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.exec.action({
+          _name: 'init',
+          parent: {
+            failFast: true,
+            regex: '.*github.*',
+            tags: 'tag1,tag2',
+            configFile: '.somerepoman.json'
+          }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
       mockFs.expects('readFileSync').returns('{}');
-      execStub.callsFake(function (command, opts, cb) {
+      execStub.callsFake(function(command, opts, cb) {
         assert.equal(command, 'init');
         assert.equal(opts.failFast, true);
         assert.equal(opts.regex, '.*github.*');
@@ -246,25 +253,27 @@ describe('cli', function() {
   });
 
   describe('list', function() {
-
     var listStub;
 
-    beforeEach(function () {
+    beforeEach(function() {
       listStub = sinon.stub(Repoman.prototype, 'list');
     });
 
-    afterEach(function () {
+    afterEach(function() {
       Repoman.prototype.list.restore();
     });
 
-    it('should log each repo name when there is result array', function () {
-      listStub.callsFake(function (opts, cb) {
+    it('should log each repo name when there is result array', function() {
+      listStub.callsFake(function(opts, cb) {
         cb(null, ['somerepo1', 'somerepo2']);
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.list.action({ _name: 'list', parent: { tags: 'somerepo' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.list.action({
+          _name: 'list',
+          parent: { tags: 'somerepo' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.repoman.json');
         return '{}';
       });
@@ -274,14 +283,17 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should not log anything when there is no result', function () {
-      listStub.callsFake(function (opts, cb) {
+    it('should not log anything when there is no result', function() {
+      listStub.callsFake(function(opts, cb) {
         cb(null, []);
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.list.action({ _name: 'list', parent: { configFile: '.somerepoman.json' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.list.action({
+          _name: 'list',
+          parent: { configFile: '.somerepoman.json' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
@@ -291,30 +303,32 @@ describe('cli', function() {
   });
 
   describe('clean', function() {
-
     var cleanStub;
     var mockPrompt;
 
-    beforeEach(function () {
+    beforeEach(function() {
       cleanStub = sinon.stub(Repoman.prototype, 'clean');
       mockPrompt = sinon.mock(prompt);
     });
 
-    afterEach(function () {
+    afterEach(function() {
       mockPrompt.verify();
       mockPrompt.restore();
       Repoman.prototype.clean.restore();
     });
 
-    it('should delete nothing when there is no directory to clean up', function () {
-      cleanStub.callsFake(function (dryRun, cb) {
+    it('should delete nothing when there is no directory to clean up', function() {
+      cleanStub.callsFake(function(dryRun, cb) {
         assert.equal(dryRun, true);
         cb(null, null);
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.clean.action({
+          _name: 'clean',
+          parent: { configFile: '.somerepoman.json' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
@@ -323,77 +337,119 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should delete directories to clean up when user confirms to do so', function () {
-      mockPrompt.expects('start').once().withExactArgs();
-      mockPrompt.expects('get').once().withArgs(['Are you sure? (Y/N)']).callsArgWith(1, null, { 'Are you sure? (Y/N)': 'Y' });
-      cleanStub.callsFake(function (dryRun, cb) {
+    it('should delete directories to clean up when user confirms to do so', function() {
+      mockPrompt
+        .expects('start')
+        .once()
+        .withExactArgs();
+      mockPrompt
+        .expects('get')
+        .once()
+        .withArgs(['Are you sure? (Y/N)'])
+        .callsArgWith(1, null, { 'Are you sure? (Y/N)': 'Y' });
+      cleanStub.callsFake(function(dryRun, cb) {
         if (dryRun) {
           cb(null, ['dir1', 'dir2']);
         } else {
           cb();
         }
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.clean.action({
+          _name: 'clean',
+          parent: { configFile: '.somerepoman.json' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
       cli.exec();
-      sinon.assert.calledWith(console.log, 'The following files/directories will be deleted: %s', 'dir1, dir2');
+      sinon.assert.calledWith(
+        console.log,
+        'The following files/directories will be deleted: %s',
+        'dir1, dir2'
+      );
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should not delete directories to clean up when user confirms to not clean', function () {
-      mockPrompt.expects('start').once().withExactArgs();
-      mockPrompt.expects('get').once().withArgs(['Are you sure? (Y/N)']).callsArgWith(1, null, { 'Are you sure? (Y/N)': 'N' });
-      cleanStub.callsFake(function (dryRun, cb) {
+    it('should not delete directories to clean up when user confirms to not clean', function() {
+      mockPrompt
+        .expects('start')
+        .once()
+        .withExactArgs();
+      mockPrompt
+        .expects('get')
+        .once()
+        .withArgs(['Are you sure? (Y/N)'])
+        .callsArgWith(1, null, { 'Are you sure? (Y/N)': 'N' });
+      cleanStub.callsFake(function(dryRun, cb) {
         assert.equal(dryRun, true);
         cb(null, ['dir1', 'dir2']);
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.clean.action({
+          _name: 'clean',
+          parent: { configFile: '.somerepoman.json' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
       cli.exec();
-      sinon.assert.calledWith(console.log, 'The following files/directories will be deleted: %s', 'dir1, dir2');
+      sinon.assert.calledWith(
+        console.log,
+        'The following files/directories will be deleted: %s',
+        'dir1, dir2'
+      );
       sinon.assert.calledWith(console.log, 'Nothing is deleted');
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should not delete directories to clean up when user does not provide confirmation answer', function () {
-      mockPrompt.expects('start').once().withExactArgs();
-      mockPrompt.expects('get').once().withArgs(['Are you sure? (Y/N)']).callsArgWith(1, null, { 'Are you sure? (Y/N)': '' });
-      cleanStub.callsFake(function (dryRun, cb) {
+    it('should not delete directories to clean up when user does not provide confirmation answer', function() {
+      mockPrompt
+        .expects('start')
+        .once()
+        .withExactArgs();
+      mockPrompt
+        .expects('get')
+        .once()
+        .withArgs(['Are you sure? (Y/N)'])
+        .callsArgWith(1, null, { 'Are you sure? (Y/N)': '' });
+      cleanStub.callsFake(function(dryRun, cb) {
         assert.equal(dryRun, true);
         cb(null, ['dir1', 'dir2']);
       });
-      commandStub.callsFake(function (base, actions) {
-        actions.commands.clean.action({ _name: 'clean', parent: { configFile: '.somerepoman.json' } });
+      commandStub.callsFake(function(base, actions) {
+        actions.commands.clean.action({
+          _name: 'clean',
+          parent: { configFile: '.somerepoman.json' }
+        });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.somerepoman.json');
         return '{}';
       });
       cli.exec();
-      sinon.assert.calledWith(console.log, 'The following files/directories will be deleted: %s', 'dir1, dir2');
+      sinon.assert.calledWith(
+        console.log,
+        'The following files/directories will be deleted: %s',
+        'dir1, dir2'
+      );
       sinon.assert.calledWith(console.log, 'Nothing is deleted');
       sinon.assert.calledWith(process.exit, 0);
     });
 
-    it('should pass error to callback when there is an error during dry run', function () {
-      cleanStub.callsFake(function (dryRun, cb) {
+    it('should pass error to callback when there is an error during dry run', function() {
+      cleanStub.callsFake(function(dryRun, cb) {
         assert.equal(dryRun, true);
         cb(new Error('some error'));
       });
-      commandStub.callsFake(function (base, actions) {
+      commandStub.callsFake(function(base, actions) {
         actions.commands.clean.action({ _name: 'clean', parent: {} });
       });
-      lookupFileStub.callsFake(function (file) {
+      lookupFileStub.callsFake(function(file) {
         assert.equal(file, '.repoman.json');
         return '{}';
       });
@@ -402,6 +458,4 @@ describe('cli', function() {
       sinon.assert.calledWith(process.exit, 1);
     });
   });
-
 });
-
--- test/generator/local.js
+++ test/generator/local.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var fs      = require('fs');
-var ini     = require('ini');
-var Local   = require('../../lib/generator/local');
+var fs = require('fs');
+var ini = require('ini');
+var Local = require('../../lib/generator/local');
 var svnInfo = require('svn-info');
 
 var mocha = require('mocha');
@@ -11,20 +11,18 @@ var sinon = require('sinon');
 var assert = chai.assert;
 
 describe('local', function() {
-
   describe('generate', function() {
-
     var fsMock;
     var iniMock;
     var svnInfoMock;
 
-    beforeEach(function () {
+    beforeEach(function() {
       fsMock = sinon.mock(fs);
       iniMock = sinon.mock(ini);
       svnInfoMock = sinon.mock(svnInfo);
     });
 
-    afterEach(function () {
+    afterEach(function() {
       fsMock.verify();
       fsMock.restore();
       iniMock.verify();
@@ -33,20 +31,43 @@ describe('local', function() {
       svnInfoMock.restore();
     });
 
-    it('should construct config of git repos using git config remote origin', function (done) {
-
-      fsMock.expects('readdirSync').withExactArgs('somedir').returns(['repo1', 'repo2']);
-      fsMock.expects('existsSync').withExactArgs('repo1/.git/config').returns(true);
-      fsMock.expects('existsSync').withExactArgs('repo2/.git/config').returns(true);
-      fsMock.expects('readFileSync').withExactArgs('repo1/.git/config', 'utf-8').returns('somedata1');
-      fsMock.expects('readFileSync').withExactArgs('repo2/.git/config', 'utf-8').returns('somedata2');
-      iniMock.expects('parse').withExactArgs('somedata1').returns({ 'remote "origin"': { url: 'http://github.com/some/repo1' }});
-      iniMock.expects('parse').withExactArgs('somedata2').returns({ 'remote "origin"': { url: 'http://github.com/some/repo2' }});
+    it('should construct config of git repos using git config remote origin', function(done) {
+      fsMock
+        .expects('readdirSync')
+        .withExactArgs('somedir')
+        .returns(['repo1', 'repo2']);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo1/.git/config')
+        .returns(true);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo2/.git/config')
+        .returns(true);
+      fsMock
+        .expects('readFileSync')
+        .withExactArgs('repo1/.git/config', 'utf-8')
+        .returns('somedata1');
+      fsMock
+        .expects('readFileSync')
+        .withExactArgs('repo2/.git/config', 'utf-8')
+        .returns('somedata2');
+      iniMock
+        .expects('parse')
+        .withExactArgs('somedata1')
+        .returns({
+          'remote "origin"': { url: 'http://github.com/some/repo1' }
+        });
+      iniMock
+        .expects('parse')
+        .withExactArgs('somedata2')
+        .returns({
+          'remote "origin"': { url: 'http://github.com/some/repo2' }
+        });
 
       var local = new Local('somedir');
 
-      local.generate(function (err, result) {
-
+      local.generate(function(err, result) {
         assert.isNull(err);
         assert.equal(result.repo1.type, 'git');
         assert.equal(result.repo1.url, 'http://github.com/some/repo1');
@@ -57,20 +78,39 @@ describe('local', function() {
       });
     });
 
-    it('should construct config of svn repos using svn info repository root', function (done) {
-
-      fsMock.expects('readdirSync').withExactArgs('somedir').returns(['repo1', 'repo2']);
-      fsMock.expects('existsSync').withExactArgs('repo1/.git/config').returns(false);
-      fsMock.expects('existsSync').withExactArgs('repo2/.git/config').returns(false);
-      fsMock.expects('existsSync').withExactArgs('repo1/.svn/entries').returns(true);
-      fsMock.expects('existsSync').withExactArgs('repo2/.svn/entries').returns(true);
-      svnInfoMock.expects('sync').withExactArgs('repo1').returns({ repositoryRoot: 'http://svnhub.com/some/repo1' });
-      svnInfoMock.expects('sync').withExactArgs('repo2').returns({ repositoryRoot: 'http://svnhub.com/some/repo2' });
+    it('should construct config of svn repos using svn info repository root', function(done) {
+      fsMock
+        .expects('readdirSync')
+        .withExactArgs('somedir')
+        .returns(['repo1', 'repo2']);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo1/.git/config')
+        .returns(false);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo2/.git/config')
+        .returns(false);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo1/.svn/entries')
+        .returns(true);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo2/.svn/entries')
+        .returns(true);
+      svnInfoMock
+        .expects('sync')
+        .withExactArgs('repo1')
+        .returns({ repositoryRoot: 'http://svnhub.com/some/repo1' });
+      svnInfoMock
+        .expects('sync')
+        .withExactArgs('repo2')
+        .returns({ repositoryRoot: 'http://svnhub.com/some/repo2' });
 
       var local = new Local('somedir');
 
-      local.generate(function (err, result) {
-
+      local.generate(function(err, result) {
         assert.isNull(err);
         assert.equal(result.repo1.type, 'svn');
         assert.equal(result.repo1.url, 'http://svnhub.com/some/repo1');
@@ -81,18 +121,31 @@ describe('local', function() {
       });
     });
 
-    it('should pass empty config when repo is neither git nor svn', function (done) {
-
-      fsMock.expects('readdirSync').withExactArgs('somedir').returns(['repo1', 'repo2']);
-      fsMock.expects('existsSync').withExactArgs('repo1/.git/config').returns(false);
-      fsMock.expects('existsSync').withExactArgs('repo2/.git/config').returns(false);
-      fsMock.expects('existsSync').withExactArgs('repo1/.svn/entries').returns(false);
-      fsMock.expects('existsSync').withExactArgs('repo2/.svn/entries').returns(false);
+    it('should pass empty config when repo is neither git nor svn', function(done) {
+      fsMock
+        .expects('readdirSync')
+        .withExactArgs('somedir')
+        .returns(['repo1', 'repo2']);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo1/.git/config')
+        .returns(false);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo2/.git/config')
+        .returns(false);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo1/.svn/entries')
+        .returns(false);
+      fsMock
+        .expects('existsSync')
+        .withExactArgs('repo2/.svn/entries')
+        .returns(false);
 
       var local = new Local('somedir');
 
-      local.generate(function (err, result) {
-
+      local.generate(function(err, result) {
         assert.isNull(err);
         assert.isEmpty(result);
 
@@ -100,5 +153,4 @@ describe('local', function() {
       });
     });
   });
-
 });
